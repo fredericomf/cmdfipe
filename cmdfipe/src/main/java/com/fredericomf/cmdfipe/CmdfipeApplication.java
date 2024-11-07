80eb9ab2e58a9f9ccd82fb19ab1b71ab36a52196
Frederico Mottinha de Figueiredo <contato@fredericomf.com>
2024-11-07 17:01:26 -0300

    🎉 First commit

---
+++ cmdfipe/src/main/java/com/fredericomf/cmdfipe/CmdfipeApplication.java
@@ -0,0 +1,13 @@
+package com.fredericomf.cmdfipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdfipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmdfipeApplication.class, args);
+	}
+
+}
---
+++ cmdfipe/src/test/java/com/fredericomf/cmdfipe/CmdfipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredericomf.cmdfipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmdfipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
